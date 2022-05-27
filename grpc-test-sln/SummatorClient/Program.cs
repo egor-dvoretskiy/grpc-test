efadec4cfeee2cbaa06c81837c91382abc3ff22d
ergo.dvoretskiy <ergo.dvoretskiy@gmail.com>
2022-05-27 14:15:10 +0300

    Add client-side streaming rpc implementation.

---
+++ grpc-test-sln/SummatorClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Protos.GrpcSummatorClient;
+
+namespace SummatorClient
+{
+    public class Program
+    {
+        static public async Task Main(String[] args)
+        {
+            Console.WriteLine("Client. Client-side streaming RPC");
+            using GrpcChannel? channel = GrpcChannel.ForAddress("https://localhost:7042");
+
+            while (channel.State != Grpc.Core.ConnectivityState.Ready)
+            {
+                channel.ConnectAsync().Wait();
+            }
+
+            var client = new Summator.SummatorClient(channel);
+
+            using var call = client.Summarize();
+
+            Random random = new Random();
+
+            int num = 30;
+            var amount = random.Next(num);
+            Console.WriteLine($"Amount of numbers: {amount}");
+
+            for (int i = 0; i < amount; i++)
+            {
+                var number = random.Next(num);
+                Console.Write(number + " ");
+                await call.RequestStream.WriteAsync(new SummRequest() { Count = number });
+                await Task.Delay(TimeSpan.FromSeconds(0.5));
+            }
+
+            await call.RequestStream.CompleteAsync();
+
+            var response = await call;
+            Console.WriteLine($"{Environment.NewLine}Summ: {response.Summ}");
+            Console.WriteLine($"{string.Join(" ", response.Nums)}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ grpc-test-sln/SummatorServer/Services/SummatorService.cs
@@ -0,0 +1,42 @@
+﻿using Google.Protobuf.Collections;
+using Grpc.Core;
+using Protos.GrpcSummator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummatorServer.Services
+{
+    public class SummatorService : Summator.SummatorBase
+    {
+        private readonly ILogger _logger;
+
+        public SummatorService(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<SummatorService>();
+        }
+
+        public override async Task<SummReply> Summarize(IAsyncStreamReader<SummRequest> requestStream, ServerCallContext context)
+        {
+            var reply = new SummReply();
+
+            int sum = 0;
+            List<int> repeated = new List<int>();        
+
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                _logger.LogInformation($"Incrementing count by {message.Count}");
+
+                sum += message.Count;
+                repeated.Add(message.Count);
+            }
+
+            reply.Summ = sum;
+            reply.Nums.Add(repeated);
+
+            return reply;
+        }
+    }
+}
