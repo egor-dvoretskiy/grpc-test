8a529f4521cec08b4d6c8c030beb134073bf8a2b
ergo.dvoretskiy <ergo.dvoretskiy@gmail.com>
2022-05-27 14:14:45 +0300

    Add simple(unary) rpc implementation.

---
+++ grpc-test-sln/Commutator-Server/Services/CommutatorService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Grpc.Core;
+using Protos.GrpcCommutatorServer;
+
+namespace CommutatorServer.Services
+{
+    public class CommutatorService : Commutator.CommutatorBase
+    {
+        private readonly ILogger _logger;
+
+        List<int> orderedList = Enumerable.Range(3, 66).ToList();
+
+        public CommutatorService(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<CommutatorService>();
+        }
+
+        public override Task<ConnectionReply> GetData(ConnectionRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"Received from client: {request.MessageRequest}");
+
+            byte[] bytesReply = orderedList
+                .SelectMany(BitConverter.GetBytes)
+                .ToArray();
+
+            _logger.LogInformation($"bytes to reply: {System.Text.Encoding.UTF8.GetString(bytesReply)}");
+
+            return Task.FromResult(new ConnectionReply() { MessageReply = "Here yours data, mfck.", Data = ByteString.CopyFrom(bytesReply) });
+        }
+    }
+}
---
+++ grpc-test-sln/Commutator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Protos.GrpcCommutatorClient;
+
+namespace CommutatorClient
+{
+    public class Program
+    {
+        static public async Task Main(String[] args)
+        {
+            Console.WriteLine("Client. Simple RPC");
+            using GrpcChannel? channel = GrpcChannel.ForAddress("https://localhost:7042");
+
+            while (channel.State != Grpc.Core.ConnectivityState.Ready)
+            {
+                channel.ConnectAsync().Wait();
+            }
+
+            var client = new Commutator.CommutatorClient(channel);
+            var reply = await client.GetDataAsync(new ConnectionRequest() { MessageRequest = "GIVE ME THE DATA" });
+
+            var data = reply.Data.ToArray();
+
+            if (data != null)
+            {
+                Console.WriteLine($"Data received: {System.Text.Encoding.UTF8.GetString(data)}");
+            }
+            else
+            {
+                Console.WriteLine("No data.");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
