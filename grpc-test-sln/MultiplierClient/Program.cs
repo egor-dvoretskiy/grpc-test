c889f393997ceecaba32cf69077a7c1d03cd07ad
ergo.dvoretskiy <ergo.dvoretskiy@gmail.com>
2022-05-27 15:20:40 +0300

    Add server-side streaming rpc implementation.

---
+++ grpc-test-sln/MultiplierClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Mult;
+using System.Threading.Tasks;
+
+namespace MultiplierClient
+{
+    public class Program
+    {
+        static public async Task Main(String[] args)
+        {
+            Console.WriteLine("Client. Server-side streaming RPC");
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:7042");
+
+            channel.ConnectAsync().Wait();
+
+            var client = new Multiplier.MultiplierClient(channel);
+
+            using var call = client.Multiply(new Google.Protobuf.WellKnownTypes.Empty());
+
+            long res = 1;
+
+            await foreach (var message in call.ResponseStream.ReadAllAsync())
+            {
+                if (message == null)
+                {
+                    Console.WriteLine("No data");
+                }
+                else {
+                    Console.Write(message.Res + " ");
+
+                    res *= message.Res;
+                }
+            }
+
+            Console.WriteLine($"{Environment.NewLine}Res: {res}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ grpc-test-sln/MultiplierServer/Services/MultiplierService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Mult;
+
+namespace MultiplierServer.Services
+{
+    public class MultiplierService : Multiplier.MultiplierBase
+    {
+        private readonly ILogger _logger;
+
+        public MultiplierService(ILoggerFactory logger)
+        {
+            _logger = logger.CreateLogger<MultiplierService>();
+        }
+
+        public override async Task Multiply(Empty request, IServerStreamWriter<MultiplyReply> responseStream, ServerCallContext context)
+        {
+            Random random = new Random();
+            int amount = random.Next(6, 24);
+            Console.WriteLine($"Numbers amount: {amount}");
+
+            long mult = 1;
+
+            for (int i = 0; i < amount; i++)
+            {
+                int num = random.Next(1, 14);
+
+                mult *= num;
+
+                Console.Write(num + " ");
+
+                await responseStream.WriteAsync(new MultiplyReply { Res = num });
+                await Task.Delay(TimeSpan.FromSeconds(0.5));
+            }
+
+            Console.WriteLine($"Mult: {mult}");
+        }
+    }
+}
